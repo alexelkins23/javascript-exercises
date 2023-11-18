55aa0d388b46ce65ddff91566f16d98837e29141
Alex Elkins <alexelkins23@gmail.com>
2023-11-17 22:40:06 -0500

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let return_str = "";
+  for (i = 0; i < num; ++i) {
+    return_str += str;
+  }
+  return return_str;
 };
 
 // Do not edit below this line
