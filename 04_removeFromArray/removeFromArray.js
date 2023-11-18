1b45d8d22d9fdd67ee65fe9506e2a4bcf335bb31
Alex Elkins <alexelkins23@gmail.com>
2023-11-17 23:11:28 -0500

    Completed exercise 04 for real

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(arr, ...args) {
   for (const arg of args) {
     const index = arr.indexOf(arg);
     if (index !== undefined) {
-      arr.splice(index, 1);
+      if (arg === arr[index]) arr.splice(index, 1);
     }
   }
   return arr;
