6ca8d130ca9430de1506d2e56875e4c0d47a4447
Alex Elkins <alexelkins23@gmail.com>
2023-11-17 23:42:37 -0500

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32) * (5/9);
+  return +cel.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fah = temp * (9/5) + 32;
+  return +fah.toFixed(1);
 };
 
 // Do not edit below this line
